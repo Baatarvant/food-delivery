b1158ca1dd0b2e575366d1b3d32c32ef177d65ed
baatarvan.t <baatarvant@gmail.com>
2026-04-03 09:51:23 +0800

    ajshd

--- client/lib/services/get-categories.ts
+++ client/lib/services/get-categories.ts
@@ -6,7 +6,7 @@ type GetCategoriesResponse = {
 
 export const getCategories = async (): Promise<GetCategoriesResponse> => {
   const response = await fetch(
-    "https://pine-food-delivery.vercel.app/categories",
+    "https://food-delivery-kohl-alpha.vercel.app/categories",
     {
       headers: {
         "Content-Type": "application/json",
--- server/dist/index.js
+++ server/dist/index.js
@@ -12,6 +12,7 @@ const app = (0, express_1.default)();
 const PORT = 3001;
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
+app.get("/", (req, res) => res.send("Hello"));
 app.use("/users", users_router_1.default);
 app.use("/orders", orders_router_1.default);
 app.use("/categories", category_router_1.default);
